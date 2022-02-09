942ece744564e8d3d248ac380f61330def203ff5
Kevin <kevin.roberts25@gmail.com>
2022-02-09 20:04:54 +0000

    Working with Services and Data - completest

---
+++ BugTrackerUI/Pages/NewBug.cs
@@ -0,0 +1,18 @@
+﻿namespace BugTrackerUI.Pages;
+
+using Microsoft.AspNetCore.Components;
+
+public partial class NewBug
+{
+    [Inject]
+    public IBugService BugService { get; set; }
+    [Inject]
+    public NavigationManager NavService { get; set; }
+    public Bug AddBug { get; set; } = new ();
+
+    protected void HandleValidSubmit()
+    {
+        BugService.AddBug(AddBug);
+    }
+
+}
